66eccb20b1f51cd98b5c7a861b22200557f3e68c
Juan Guzman <guzman.carvajal240406@gmail.com>
2022-10-25 15:50:06 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+c++
+
+// if_else_statement.cpp
+#include <iostream>
+
+using namespace std;
+
+class C
+{
+    public:
+    void do_something(){}
+};
+void init(c){}
+bool is_true() { return true; }
+init x = 10;
+
+int main()
+        {
+         if  (is_true())
+        {
+        cout << "b is true!\n"; // executed
+        }
+         else
+        {
+            cout << "b is false\n";
+        }
+
+         // no else statement
+        if (x == 10)
+        {
+            x = 0;
+        }
+
+        C* c;
+        init(c);
+        if (c)
+        {
+            c->do_something();
+        }
+        else
+        {
+            cout << "c is null!\n";
+        }
+        }
+
+
+
